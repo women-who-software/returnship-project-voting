e1826dbf4c791aa305551f39f30ee9cf580ae7a1
Kim <cableredi@comcast.net>
2020-06-27 15:50:14 -0600

    issue9 and issue6 updates

--- client/src/Components/Voting/Voting.js
+++ client/src/Components/Voting/Voting.js
@@ -243,7 +243,6 @@ export default function Voting() {
                 onChange={(e) => updateSlack(e.target.value)}
                 type="text"
                 placeholder="Enter your Slack Handle"
-                size="35"
                 required
               />
             </div>
@@ -260,7 +259,6 @@ export default function Voting() {
                 onChange={(e) => updateEmail(e.target.value)}
                 type="text"
                 placeholder="Please enter a valid Email address"
-                size="35"
               />
             </div>
             <div>
