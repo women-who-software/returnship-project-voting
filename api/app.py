513ceec49a529a60a55ddd8b24f22717be05ca95
NastyaShafir <zaichic1@yandex.ru>
2020-08-16 16:43:37 +1000

    - add projects - get all projects

--- api/app.py
+++ api/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_migrate import Migrate
 from sqlalchemy import ForeignKey
+import json
 
 
 app = Flask(__name__)
@@ -14,19 +15,23 @@ db = SQLAlchemy(app)
 
 class Project(db.Model):
     project_id = db.Column(db.Integer, primary_key=True)
-    project_name = db.Column(db.String(100), nullable=True)
+    project_name = db.Column(db.String(100))
     project_desc = db.Column(db.Text, nullable=True)
     client_name = db.Column(db.String(100), nullable=True)
     client_email = db.Column(db.String(100))
-    client_phone = db.Column(db.Integer)
-    project_status = db.Column(db.String(100))
-    project_stack = db.Column(db.String(100))
+    client_phone = db.Column(db.Integer, nullable=True)
+    project_status = db.Column(db.String(100), nullable=True)
+    project_stack = db.Column(db.String(100), nullable=True)
     max_members = db.Column(db.Integer)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+    date = db.Column(db.DateTime, default=datetime.utcnow, nullable=True)
 
 
     def __repr__(self):
         return '<Project %r>' % self.project_name
+
+    def serialize(self):
+        # todo в serialize указать все поля, даже если они пустые
+        return {'client_email': self.client_email, 'max_members': self.max_members, 'project_name': self.project_name}
 
 
 class Signup(db.Model):
@@ -58,65 +63,37 @@ class Admin(db.Model):
     def __repr__(self):
         return '<Admin %r>' % self.admin_name
 
-
 # @app.route('/')
 # def index():
 #     return "Home"
 #
 #
-# @app.route('/post-project', methods=['POST','GET'])
-# def post_project():
-#     if request.method == "POST":
-#         client_email = request.form['client_email']
-#
-#         project = Project(client_email=client_email)
-#
-#         try:
-#             db.session.add(project)
-#             db.session.commit()
-#             return redirect('/projects')
-#         except Exception as e:
-#             print(e)
-#             return "Nothing here. Try add the project again."
-#     else:
-#         return render_template("post_project.html")
-#
-#
-# @app.route('/projects')
-# def all_projects():
-#     projects = Project.query.order_by(Project.project_id).all()
-#     return render_template("all-projects.html", projects=projects)
+@app.route('/api/post-project', methods=['POST'])
+def post_project():
+    client_email = request.form['client_email']
+    max_members = request.form['max_members']
+    project_name = request.form['project_name']
+
+    # todo здесь добавить все поля
+
+    project = Project(client_email=client_email, max_members=max_members, project_name=project_name)
+
+    try:
+        db.session.add(project)
+        db.session.commit()
+        return redirect('/api/projects')
+    except Exception as e:
+        print(e)
+        return "Nothing here. Try add the project again."
 
 
-@app.route('/')
-def index():
-    return "Home"
 
-
-@app.route('/projects')
-def projects():
-    return "Project list"
-
-
-@app.route('/details')
-def details():
-    return "Project details"
-
-
-@app.route('/vote')
-def vote():
-    return "Vote"
-
-
-@app.route('/signup')
-def signup():
-    return "Sign up"
-
-
-@app.route('/admin')
-def admin():
-    return "Admin"
+@app.route('/api/projects')
+def all_projects():
+    projects = Project.query.order_by(Project.project_id).all()
+    return jsonify(list(map(lambda v: v.serialize(), projects)))
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
