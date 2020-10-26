66b468473657cb95f819465f4045c46b72bbafa5
CodeMeKathy * <Kathy@KathyLambert.me>
2020-10-26 13:14:22 -0400

    Refactor(Styling): Modify <AdminPanel /> project table and list layout

--- client/src/Components/AdminPanel/AdminPanel.js
+++ client/src/Components/AdminPanel/AdminPanel.js
@@ -17,6 +17,8 @@ export default function AdminPanel() {
 
   const getProjects = projects.map((projects) => (
     <div className="projects__items-project card" key={projects.project_id}>
+      {/* TODO: Create method function for checkbox action */}
+      <input type="checkbox" name="select_checkbox" className='project_checkbox' id=""/>
       <div className="projects__items-project-name table-col">
         <NavLink to={`/projects/${projects.project_id}`}>
           {projects.project_name}
@@ -44,6 +46,7 @@ export default function AdminPanel() {
               <a role="button" href="#" className="tool-btn bulk-btn">
                 Bulk Actions
               </a>
+              {/* TODO: Convert to accordion with options: 1) Edit 2) Move to Trash  */}
               <span className="chevronDownElement">{chevronDownElement}</span>
               <i class="fas fa-chevron-down"></i>
             </div>
@@ -59,13 +62,21 @@ export default function AdminPanel() {
             </div>
           </div>
           <div className="row table-header">
-            <div className="projects__items-project-name table-col">
+            <div className="projects__items-project-name table-col header-name">
               Project
             </div>
-            <div className="projects__items-vote table-col">Vote</div>
-            <div className="projects__items-sign_up table-col">Sign Up</div>
-            <div className="projects__items-status table-col">Status</div>
-            <div className="projects__items-date table-col">Date</div>
+            <div className="projects__items-vote table-col header-vote">
+              Vote
+            </div>
+            <div className="projects__items-sign_up table-col header-sign_up">
+              Sign Up
+            </div>
+            <div className="projects__items-status table-col header-status">
+              Status
+            </div>
+            <div className="projects__items-date table-col header-date">
+              Date
+            </div>
           </div>
           <div className="col projectCards">{getProjects}</div>
         </div>
