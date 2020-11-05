e8aff5006d315795081eeb7a234311059aeb8e82
CodeMeKathy * <Kathy@KathyLambert.me>
2020-11-05 10:00:56 -0500

    Feature (<AdminProjectDetails/>): Add routing to access project details in admin panel

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 // Import React Components
 import React, { useState, useEffect, useContext } from "react";
 import { Route, Switch } from "react-router-dom";
-import AboutPage from "./routes/aboutPage";
-import ProjectsPage from "./routes/projectsPage";
-import AdminPanel from "./Components/AdminPanel/AdminPanel";
+
+// Temporary Data
 import STORE from "./STORE";
 
 // State Management
 import { GlobalContext } from "./Context/GlobalContext";
 import NotFound from "./routes/notFoundPage";
 
+// Import Custom Components
+import AboutPage from "./routes/aboutPage";
+import ProjectsPage from "./routes/projectsPage";
+import AdminPanel from "./Components/AdminPanel/AdminPanel";
+import AdminProjectDetails from "./Components/AdminProjectDetails/AdminProjectDetails";
 export default function App() {
   const [hasError, setHasError] = useState("");
   const { projects, setProjects } = useContext(GlobalContext);
@@ -26,6 +30,19 @@ export default function App() {
           <Route exact path="/" component={AboutPage} />
           <Route exact path="/projects" component={ProjectsPage} />
           <Route exact path="/admin" component={AdminPanel} />
+          <Route
+            exact
+            path="/admin/projects/:project_id"
+            component={(routeProps) => (
+              <AdminProjectDetails
+                project={projects.find(
+                  (project) =>
+                    project.project_id ===
+                    Number(routeProps.match.params.project_id)
+                )}
+              />
+            )}
+          />
           <Route component={NotFound} />
         </Switch>
       </main>
