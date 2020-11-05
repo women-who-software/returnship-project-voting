b2f36eb53668013bebbb2cccf17fb35ff801c762
CodeMeKathy * <Kathy@KathyLambert.me>
2020-11-05 12:34:35 -0500

    Refactor (<SideBar />): Update icon image links

--- client/src/Components/SideBar/SideBar.js
+++ client/src/Components/SideBar/SideBar.js
@@ -11,7 +11,7 @@ const SideBar = () => (
             <li>
               <NavLink className="sideBar--link" exact to="/admin">
                 <img
-                  src="https://github.com/wwcodecolorado/returnship-project-voting/blob/kl-feature12-admin-panel-redesign/client/assets/icons/archives.png?raw=true"
+                  src="https://github.com/wwcodecolorado/returnship-project-voting/blob/master/client/assets/icons/archives.png?raw=true"
                   alt="Archives Icon"
                   className="admin-header--icon"
                   id="admin-header--archives-icon"
@@ -22,7 +22,7 @@ const SideBar = () => (
             <li>
               <NavLink className="sideBar--link" exact to="/admin">
                 <img
-                  src="https://github.com/wwcodecolorado/returnship-project-voting/blob/kl-feature12-admin-panel-redesign/client/assets/icons/user-icon.png?raw=true"
+                  src="https://github.com/wwcodecolorado/returnship-project-voting/blob/master/client/assets/icons/user-icon.png?raw=true"
                   alt="User Icon"
                   className="admin-header--icon"
                 />
