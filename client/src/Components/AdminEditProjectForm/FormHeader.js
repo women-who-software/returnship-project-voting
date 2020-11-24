fe3bd978f769f656e4b4bee2fdea6af383e7a503
CodeMeKathy * <Kathy@KathyLambert.me>
2020-11-24 11:55:06 -0500

    Style (<FormHeader />): Add styling to match mockup design

--- client/src/Components/AdminEditProjectForm/FormHeader.js
+++ client/src/Components/AdminEditProjectForm/FormHeader.js
@@ -9,29 +9,34 @@ export default function FormHeader(props) {
   });
   return (
     <>
-      <div className="adminEditProjectForm__form_row adminEditProjectForm__header">
-        <div className="adminEditProjectForm__form_col adminEditProjectForm__header_title">
-          Project Details
+      <div className="adminEditProjectForm__header">
+        <div className="adminEditProjectForm__form_row adminEditProjectForm__toolbar">
+          <div className="adminEditProjectForm__form_col adminEditProjectForm__header_title">
+            Project Details
+          </div>
+          <div className="adminEditProjectForm__form_col adminEditProjectForm__btn_col">
+            <button className="adminEditProjectForm__btn">Save As Draft</button>
+          </div>
+          <div className="adminEditProjectForm__form_col adminEditProjectForm__btn_col">
+            <button className="adminEditProjectForm__btn" type="submit">
+              Publish
+            </button>
+          </div>
         </div>
-        <div className="adminEditProjectForm__form_col">
-          <button className="adminEditProjectForm__btn">Save As Draft</button>
+        <div className="adminEditProjectForm__project_info_container">
+          <div className="adminEditProjectForm__form_row adminEditProjectForm__project_title">
+            <label>Project Title:</label>
+          </div>
+          <div className="adminEditProjectForm__form_row">
+            <input
+              type="text"
+              name="project_name"
+              className="adminEditProjectForm__project_title_input"
+              defaultValue={projectName.value}
+              // onChange={}
+            />
+          </div>
         </div>
-        <div className="adminEditProjectForm__form_col">
-          <button className="adminEditProjectForm__btn" type="submit">
-            Publish
-          </button>
-        </div>
-      </div>
-      <div className="adminEditProjectForm__form_row">
-        <label>Project Title</label>
-      </div>
-      <div className="adminEditProjectForm__form_row">
-        <input
-          type="text"
-          name="project_name"
-          defaultValue={projectName.value}
-          // onChange={}
-        />
       </div>
     </>
   );
